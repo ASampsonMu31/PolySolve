e14f97c9dae50de8ed3918295c62cbfe10a9adf5
Sampson <alex.sampson@stfc.ac.uk>
2025-02-14 15:36:51 +0000

    Wrote docstrings and typehints

--- polysolve/polysolve.py
+++ polysolve/polysolve.py
@@ -1,15 +1,64 @@
+from __future__ import annotations
 import numpy as np
 
 CBRT_UNITY_IM = np.sqrt(3)/2 * 1j
 
 
-def solve_quadratic(a, b, c):
+def solve_quadratic(
+        a: float,
+        b:float,
+        c:float,
+) -> tuple[float, float]:
+    """
+    Solves the roots of a quadratic equation.
+    Uses the quadratic formula. Result must be real.
+
+    Parameters
+    ----------------
+    a
+        :math: 'x^2' coefficient.
+    b
+        :math: 'x' coefficient
+    c
+        Constant value.
+        
+    Returns
+    ---------
+    tuple[float, float]
+        Positive and negative roots of the quadratic.
+    """
+    
     det = b**2 - (4*a*c)
 
     return ((-b + np.sqrt(det)) / (2*a),
             (-b - np.sqrt(det)) / (2*a))
 
-def solve_cubic(a, b, c, d):
+def solve_cubic(
+        a: float,
+        b: float,
+        c: float,
+        d: float,
+) -> tuple[float, float, float]:
+    """
+    Solves the roots of a cubic equation.
+    Uses the cubic formula. Result must be real.
+
+    Parameters
+    ----------------
+    a
+        :math: 'x^3' coefficient.
+    b
+        :math: 'x^2' coefficient
+    c
+        :math: 'x' coefficient
+    d
+        Constant value.
+        
+    Returns
+    ---------
+    tuple[float, float]
+        Positive and negative roots of the cubic.
+    """
     q = (3*a*c - b**2) / (9*a**2)
     r = (9*a*b*c - 27*a**2*d - 2*b**3) / (54*a**3)
 
